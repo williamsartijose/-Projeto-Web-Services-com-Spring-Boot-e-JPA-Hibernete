fe8c71e69345f0559338fe224a1a906f6a7aa549
williamsartijose <williamsartijose@hotmail.com>
2022-01-18 10:21:20 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/williamsarti/ProjetoSpring/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.williamsarti.ProjetoSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.williamsarti.ProjetoSpring.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
